a020f7c0ecec19f94e305729929914bcd63227c7
Alliya dhiraj <alliyadhirajsubhahchandani@gmail.com>
2023-02-23 16:19:51 +0530

    loadding bar

--- src/App.js
+++ src/App.js
@@ -3,17 +3,48 @@ import './App.css';
 import React, { Component } from 'react'
 import { Navbar } from './component/Navbar';
 import News from './component/News';
+import LoadingBar from 'react-top-loading-bar'
 
+
+import { Route, Routes } from 'react-router-dom';
 
 export default class App extends Component {
 
+  pageSize = 5;
+
+  state = {
+    progress: 0
+  }
+
+  setProgress = (progress)=>{
+    this.setState({
+      progress: progress
+    })
+  }
+
   render() {
     return (
-      <div>
-          <Navbar/> 
-          <News pageSize={5}/>
-                 
-      </div>
+      <>
+        <Navbar />
+
+        <LoadingBar
+        height={3}
+        color='#f11946'
+        progress={this.state.progress}
+        
+      />
+
+        <Routes>
+          <Route exact path="/" element={<News setProgress={this.setProgress} key="general" pageSize={5} country='in' category='general' />}></Route>
+          <Route exact path="/business" element={<News setProgress={this.setProgress} key="business" pageSize={5} country='in' category='business' />}></Route>
+          <Route exact path="/entertainment" element={<News setProgress={this.setProgress} key="entertainment" pageSize={5} country='in' category='entertainment' />}></Route>
+          <Route exact path="/general" element={<News setProgress={this.setProgress} key="general" pageSize={5} country='in' category='general' />}></Route>
+          <Route exact path="/health" element={<News setProgress={this.setProgress} key="health" pageSize={5} country='in' category='health' />}></Route>
+          <Route exact path="/science" element={<News setProgress={this.setProgress} key="science" pageSize={5} country='in' category='science' />}></Route>
+          <Route exact path="/sports" element={<News setProgress={this.setProgress} key="sports" pageSize={5} country='in' category='sports' />}></Route>
+          <Route exact path="/technology" element={<News setProgress={this.setProgress} key="tecnology" pageSize={5} country='in' category='technology' />}></Route>
+        </Routes>
+      </>
     )
   }
 }
--- src/component/NewsItem.js
+++ src/component/NewsItem.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title, description, imageUrl, newsUrl} = this.props;
+    let {title, description, imageUrl, newsUrl, author, date} = this.props;
     return (
       <div className='my-3'>
-        <div className="card" style={{width: "18rem"}}>
+        <div className="card">
           <img src={!imageUrl?"https://aniportalimages.s3.amazonaws.com/media/details/ANI-20230110073128.jpg":imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
+            <p className='card-text'><small className='text-muted'>By {!author?"Unknow" : author} on {date}</small></p>
             <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-dark btn-sm ">Read More</a>
           </div>
         </div>
